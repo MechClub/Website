6beeec01f90f853bca0d2e6896461fa7d4beeaab
MapleSpace <maplespace@proton.me>
2024-11-18 17:02:21 -0500

    Mech Club Website V1.0
    - Finally made the Website Official!!
    - Added info on mech mentorship and changed a few things in the other
      channels
    - Website should now (or very soon) be hosted on https://mech.skule.ca/

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: 'https://mechclub.github.io',
+  site: 'https://mech.skule.ca',
 });
